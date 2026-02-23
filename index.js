035281e90f7c75fe44fd2e14cc47763ef678373f
surajcvraman2025-del <surajc.vraman2025@gmail.com>
2026-02-23 11:31:07 +0530

    this commit for put element in var and change counter value

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 let A = 0;
-
+const counter_value = document.getElementById("counter_value")
 
 function Increment_fun() {
    A = A + 1;
    console.log(A);
-     document.getElementById("counter_value").innerText = A;
+    //  document.getElementById("counter_value").innerText = A;
+    counter_value.innerText = A
 
     
 }
  function decreas_fun() {
   A = A - 1;
   console.log(A);
-  document.getElementById("counter_value").innerText = A;
+  // type of changing element first is thake direct 
+  // document.getElementById("counter_value").innerText = A;
+  counter_value.innerText = A //second create a var or const and put element in varable 
  }saa
