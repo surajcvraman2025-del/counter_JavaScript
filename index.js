687453b629ef2e57d1369d7e4267d83c3ae50257
surajcvraman2025-del <surajc.vraman2025@gmail.com>
2026-02-23 12:11:53 +0530

    comiit for change css + add save buttom

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-let A = 0;
-const counter_value = document.getElementById("counter_value")
+// let A = 0;
+// const counter_value = document.getElementById("counter_value")
 
-function Increment_fun() {
-   A = A + 1;
-   console.log(A);
-    //  document.getElementById("counter_value").innerText = A;
-    counter_value.innerText = A
+// function Increment_fun() {
+//    A = A + 1;
+//    console.log(A);
+//     //  document.getElementById("counter_value").innerText = A;
+//     counter_value.innerText = A
 
     
+// }
+//  function decreas_fun() {
+//   A = A - 1;
+//   console.log(A);
+//   // type of changing element first is thake direct 
+//   // document.getElementById("counter_value").innerText = A;
+//   counter_value.innerText = A //second create a var or const and put element in varable 
+//  }
+//   function Save_Out() {
+//     console.log(A)
+
+
+//   }
+//   Save_Out();
+
+  let count = 0
+let counterValue = document.getElementById("counter_value")
+
+function incrementFun() {
+    count++
+    updateCounter()
 }
- function decreas_fun() {
-  A = A - 1;
-  console.log(A);
-  // type of changing element first is thake direct 
-  // document.getElementById("counter_value").innerText = A;
-  counter_value.innerText = A //second create a var or const and put element in varable 
- }saa
+
+function decreaseFun() {
+    count--
+    updateCounter()
+}
+
+function saveOut() {
+    //console.log("Saved value: " + count)
+    let Save = count
+    let SaveElement = document.getElementById("Save_buttom")
+    console.log(Save)
+    SaveElement.innerText = `this is totel value ${count} `
+    }
+
+function updateCounter() {
+    counterValue.textContent = count
+
+    // Add pop animation
+    counterValue.classList.add("pop")
+
+    // Remove animation class after it finishes
+    setTimeout(() => {
+        counterValue.classList.remove("pop")
+    }, 200)
+}
